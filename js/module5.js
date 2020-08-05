16c150a8f74dd648d34fe06e61f997a2322b5821
IvannaMelnyk <givmiller@gmail.com>
2020-08-05 09:02:59 +0300

    m 6
    Перебор масива array возвращается  numbers. Умножение  на 10

--- js/module5.js
+++ js/module5.js
@@ -1,55 +1,17 @@
-const countryName = "индия";
+function checkForSpam(str) {
+    'use strict';
+    // Write code under this line
+    const string = str.toLowerCase();
+    const checkedString =
+        string.includes('spam') || string.includes('sale') ? true : false;
 
-const CANCELED_BY_USER = 'Отменено пользователем!';
-const NO_DELIVERY = 'В выбранную страну доставка недоступна.';
-const CHINA = 'Китай';
-const AUSTRALIA = 'Австралия';
-const INDIA = 'Индия';
-const JAMAICA = 'Ямайка';
-let message;
-let price = 0;
-let country;
-if (countryName === null) {
-    message = CANCELED_BY_USER;
-} else {
-    country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase(); // Write code on this line
-    switch (country) {
-        case 'Китай':
-            price = 100;
-            country = CHINA;
-            break;
-        case 'Австралия':
-            price = 170;
-            country = AUSTRALIA;
-            break;
-        case 'Индия':
-            price = 80;
-            country = INDIA;
-            break;
-        case 'Ямайка':
-            price = 120;
-            country = JAMAICA;
-            break;
-        default:
-            message = NO_DELIVERY;
-        // Write code under this line
-
-    }
-}
-if (price > 0) { // Write code on this line
-    message = `Доставка в ${country} будет стоить ${price} кредитов`;
+    return checkedString;
 }
 
-console.log(message);
+console.log(checkForSpam('Latest technology news')); // false
 
-//если countryName равно "КитаЙ"
-// то значение message будет равно
-// 'Доставка в Китай будет стоить 100 кредитов'
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
 
-//если countryName равно null
-// то значение message будет равно
-// 'Отменено пользователем!'
+console.log(checkForSpam('Get best sale offers now!')); // true
 
-//если countryName равно "Чили"
-// то значение message будет равно
-// 'В выбранную страну доставка недоступна.'
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
